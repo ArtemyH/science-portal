50e637d917c3460cc491b398ec12cfc0585f420a
Artemy H <hanov_artem@idpowers.com>
2018-04-24 01:39:34 +0300

    chore: статика для heroku

--- server/core/settings/base.py
+++ server/core/settings/base.py
@@ -111,7 +111,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, '../../static'))
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
 STATICFILES_DIRS = (
     os.path.normpath(os.path.join(BASE_DIR, '../client/src')),
 )
