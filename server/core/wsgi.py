c949fea275a2d42dc696939dbf6bded04ab96c19
Artemy H <hanov_artem@idpowers.com>
2018-04-24 01:05:33 +0300

    chore(server): настройка деплоя на heroku

--- server/core/wsgi.py
+++ server/core/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.heroku")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.core.settings.heroku")
 
 application = get_wsgi_application()
