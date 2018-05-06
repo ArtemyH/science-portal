3668419b632d28699b5774aa7c021f0160f952f0
Artemy H <hanov_artem@idpowers.com>
2018-05-06 15:28:24 +0300

    feat(user,templates): добавлена страница редактироания профиля

--- server/apps/user/urls.py
+++ server/apps/user/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 
+from apps.user.views import UserEditView
+
+
 urlpatterns = [
+    path('accounts/profile/', UserEditView.as_view(), name='user_edit'),
     path('accounts/', include('allauth.urls')),
 ]
---
+++ server/apps/user/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
+
+
+User = get_user_model()
+
+
+class UserEditView(LoginRequiredMixin, UpdateView):
+    model = User
+    fields = ['last_name', 'first_name', 'middle_name']
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        # возвращаемся на страницу, с которой пришли
+        return self.request.get_full_path()
